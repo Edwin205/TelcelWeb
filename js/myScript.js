21b09a380cc227fc06b285b35c2ce306adea030a
Edwin_Leon <edwinleon.205@gmail.com>
2015-07-24 17:30:41 -0700

    Botones xSemana xDia xMes funcionando

--- js/myScript.js
+++ js/myScript.js
@@ -57,12 +57,17 @@ var totalMBValue2;
 var totalMBValue3;
 var totalMBValue4;
 var totalMBValue5;
-
- var usuarioLength =1;
+var valorMultiFoto = .50;
+var valorMultiVideo = 25;
+var valorMultiCancion = 7;
+var valorMultiEmail = 0.05;
+var valorMultiDocuments = 3;
+var valorMultiAplication = 300;
+var usuarioLength =1;
 
  
 function moveBar(){
-    totalMBValue0 = (photoMB + songMB + videoMb + internetMb + aplicationMb + emailMb + documentsMb )*30;
+    totalMBValue0 = (photoMB + songMB + videoMb + internetMb + aplicationMb + emailMb + documentsMb)*30;
     totalMBValue1 = (photoMB1 + songMB1 + videoMb1 + internetMb1 + aplicationMb1 + emailMb1 + documentsMb1)*30;
     totalMBValue2 = (photoMB2 + songMB2 + videoMb2 + internetMb2 + aplicationMb2 + emailMb2 + documentsMb2)*30;
     totalMBValue3 = (photoMB3 + songMB3 + videoMb3 + internetMb3 + aplicationMb3 + emailMb3 + documentsMb3)*30;
@@ -112,13 +117,123 @@ function moveBar(){
 
 $(document).ready(function(){
 
-  $( "#xDiaFoto" ).prop( "checked", true );
-$( "#xDiaCancion" ).prop( "checked", true );
+$( "#xDiaFoto0" ).prop( "checked", true );
+$( "#xDiaCancion0" ).prop( "checked", true );
+$( "#xDiaAplication0" ).prop( "checked", true );
+$( "#xDiaDocuments0" ).prop( "checked", true );
+$( "#xDiaEmail0" ).prop( "checked", true );
 
 
 
+
+
+   $('#xDiaFoto0').click(function(){
+    valorMultiFoto = .5;
+    valorMultiVideo = 25;
+    photoMB = $('#photo-stepper0').val() * valorMultiFoto;
+    videoMb = $('#video-stepper0').val() * valorMultiVideo;
+    moveBar();
+    
+   });
+
+    $('#xSemanaFoto0').click(function(){
+      valorMultiFoto = .07;
+      valorMultiVideo = 3.34;
+     photoMB = $('#photo-stepper0').val() * valorMultiFoto;
+     videoMb = $('#video-stepper0').val() * valorMultiVideo;
+     moveBar();
+   
+   });
+
+     $('#xMesFoto0').click(function(){
+      valorMultiFoto = .0167;
+      valorMultiVideo = .8334;
+      photoMB = $('#photo-stepper0').val() * valorMultiFoto;
+      videoMb = $('#video-stepper0').val() * valorMultiVideo;
+      moveBar();
+   });
+
+    $('#xDiaCancion0').click(function(){
+    valorMultiCancion = 7 ;  
+    songMB = $( "#song-slide0" ).slider("value")  *  valorMultiCancion ;
+    moveBar();
+   });
+
+    $('#xSemanaCancion0').click(function(){
+    valorMultiCancion = .93333333333;  
+      songMB = $( "#song-slide0" ).slider("value")  *  valorMultiCancion ;
+      moveBar();
+   });
+
+    $('#xMesCancion0').click(function(){
+    valorMultiCancion = .2334 ;  
+      songMB = $( "#song-slide0" ).slider("value")  * valorMultiCancion ;
+      moveBar();
+   });
+
+
+     $('#xDiaAplication0').click(function(){
+    valorMultiAplication = 300 ;  
+    aplicationMb = $( "#aplication-slide0" ).slider("value")  *  valorMultiAplication ;
+    moveBar();
+   });
+
+    $('#xSemanaAplication0').click(function(){
+      valorMultiAplication = 40;  
+      aplicationMb = $( "#aplication-slide0" ).slider("value")  *  valorMultiAplication ;
+      moveBar();
+   });
+
+    $('#xMesAplication0').click(function(){
+    valorMultiAplication = 10;  
+      aplicationMb = $( "#aplication-slide0" ).slider("value")  * valorMultiAplication ;
+      moveBar();
+   });
+
+
+    $('#xDiaDocuments0').click(function(){
+    valorMultiDocuments = 3 ;  
+    documentsMb = $( "#documents-slide0" ).slider("value")  *  valorMultiDocuments ;
+    moveBar();
+   });
+
+    $('#xSemanaDocuments0').click(function(){
+      valorMultiDocuments = 0.4;  
+      documentsMb = $( "#documents-slide0" ).slider("value")  *  valorMultiDocuments ;
+      moveBar();
+   });
+
+    $('#xMesDocuments0').click(function(){
+    valorMultiDocuments = 0.1;  
+      documentsMb = $( "#documents-slide0" ).slider("value")  * valorMultiDocuments ;
+      moveBar();
+   });
+
+    $('#xDiaEmail0').click(function(){
+    valorMultiEmail = 0.05;  
+    emailMb = $( "#email-slide0" ).slider("value")  *  valorMultiEmail ;
+    moveBar();
+   });
+
+    $('#xSemanaEmail0').click(function(){
+      valorMultiEmail = 0.007;  
+      emailMb = $( "#email-slide0" ).slider("value")  *  valorMultiEmail ;
+      moveBar();
+   });
+
+    $('#xMesEmail0').click(function(){
+    valorMultiEmail = 0.0017;  
+      emailMb = $( "#email-slide0" ).slider("value")  * valorMultiEmail ;
+      moveBar();
+   });
+
+
+
+
+/*************Usuario0*****************************/
+  
 $('#photo-steppe0').inputStepper({
-    initialValue:5 ,
+    initialValue:4,
     min: 0,
     max: Infinity,
     selectorInput: 'input',
@@ -130,56 +245,68 @@ $('#photo-steppe0').inputStepper({
 });
 
 $('#photo-stepper0').on('increase', function (e, amount, plugin) {
-    photoMB = $('#photo-stepper0').val() *.50;
+    photoMB = $('#photo-stepper0').val() * valorMultiFoto;
     moveBar();
 })
 
 $('#photo-stepper0').on('decrease', function (e, amount, plugin) {
-    photoMB = $('#photo-stepper0').val() *.50;
+    photoMB = $('#photo-stepper0').val() * valorMultiFoto;
     moveBar();
 });
 
-/*************Usuario0*****************************/
-  $('#photo-slide0').slider({
+ 
+  
+$('#video-steppe0').inputStepper({
+    initialValue:3 ,
     min: 0,
-    max: 10,
-    value:8,
-    range:"min",
-    slide:function(event,ui){
-      photoMB = ui.value * .50 ;
-      moveBar();
-    }
-    });
+    max: Infinity,
+    selectorInput: 'input',
+    selectorButtonIncrease: '[data-input-stepper-increase]',
+    selectorButtonDecrease: '[data-input-stepper-decrease]',
+    dataAttributeIncrease: 'input-stepper-increase',
+    dataAttributeDecrease: 'input-stepper-decrease',
+    classNameDisabled: 'is-disabled'
+});
 
-  $('#song-slide0').slider({
+$('#video-stepper0').on('increase', function (e, amount, plugin) {
+    videoMb = $('#video-stepper0').val() * valorMultiVideo;
+    moveBar();
+})
+
+$('#video-stepper0').on('decrease', function (e, amount, plugin) {
+    videoMb = $('#video-stepper0').val() * valorMultiVideo;
+    moveBar();
+});
+
+  $('#internet-steppe0').inputStepper({
+    initialValue:5.00 ,
+    min: 0,
+    max: Infinity,
+    selectorInput: 'input',
+    selectorButtonIncrease: '[data-input-stepper-increase]',
+    selectorButtonDecrease: '[data-input-stepper-decrease]',
+    dataAttributeIncrease: 'input-stepper-increase',
+    dataAttributeDecrease: 'input-stepper-decrease',
+    classNameDisabled: 'is-disabled'
+});
+
+$('#internet-stepper0').on('increase', function (e, amount, plugin) {
+    internetMb = $('#internet-stepper0').val() * .5;
+    moveBar();
+})
+
+$('#internet-stepper0').on('decrease', function (e, amount, plugin) {
+    internetMb = $('#internet-stepper0').val() * .5;
+    moveBar();
+});
+
+ $('#song-slide0').slider({
     min: 0,
     max: 10,
     value:5,
     range:"min",
     slide:function(event,ui){
-      songMB = ui.value * 7 ;
-      moveBar();
-    }
-  });
-
-  $('#video-slide0').slider({
-    min: 0,
-    max: 10,
-    value:5,
-    range:"min",
-    slide:function(event,ui){
-      videoMb = ui.value * 25 ;
-      moveBar();
-    }
-  });
-
-  $('#internet-slide0').slider({
-    min: 0,
-    max: 10,
-    value:5,
-    range:"min",
-    slide:function(event,ui){
-      internetMb = ui.value * .5 ;
+      songMB = ui.value * valorMultiCancion ;
       moveBar();
     }
   });
@@ -190,18 +317,18 @@ $('#photo-stepper0').on('decrease', function (e, amount, plugin) {
     value:0,
     range:"min",
     slide:function(event,ui){
-      aplicationMb = ui.value * 300 ;
+      aplicationMb = ui.value * valorMultiAplication ;
       moveBar();
     }
   });
 
   $('#email-slide0').slider({
     min: 0,
-    max: 10,
+    max: 1000,
     value:5,
     range:"min",
     slide:function(event,ui){
-      emailMb = ui.value * .05 ;
+      emailMb = ui.value * valorMultiEmail ;
       moveBar();
     }
   });
@@ -212,7 +339,7 @@ $('#photo-stepper0').on('decrease', function (e, amount, plugin) {
     value:5,
     range:"min",
     slide:function(event,ui){
-      documentsMb = ui.value * 3 ;
+      documentsMb = ui.value * valorMultiDocuments ;
       moveBar();
     }
   });
@@ -220,9 +347,9 @@ $('#photo-stepper0').on('decrease', function (e, amount, plugin) {
 
 
    photoMB = $('#photo-stepper0').val() *.50;
-   songMB = $( "#song-slide0" ).slider("value")  * 6;
-   videoMb = $( "#video-slide0" ).slider("value") * 25;
-   internetMb = $( "#internet-slide0" ).slider("value")  * .5;
+   songMB = $( "#song-slide0" ).slider("value")  * 7;
+   videoMb = $( "#video-stepper0" ).val() * 25;
+   internetMb = $('#internet-stepper0').val() * .5;
    aplicationMb = $( "#aplication-slide0" ).slider("value" ) * 300;
    emailMb = $( "#email-slide0" ).slider("value") * .05;
    documentsMb =$( "#documents-slide0" ).slider("value") * 3;
@@ -236,18 +363,43 @@ $('#photo-stepper0').on('decrease', function (e, amount, plugin) {
 
  
 function sliders(){
+  
  /*************Usuario1*****************************/
   $('#photo-stepper1').on('increase', function (e, amount, plugin) {
-      photoMB1 = $('#photo-stepper1').val() *.50;
-      moveBar();
-      totalmb1();
-  });
+    photoMB1 = $('#photo-stepper1').val() *.50;
+    moveBar();
+    totalmb1();
+})
 
-  $('#photo-stepper1').on('decrease', function (e, amount, plugin) {
-      photoMB1 = $('#photo-stepper1').val() *.50;
-      moveBar();
-      totalmb1();
-  });
+$('#photo-stepper1').on('decrease', function (e, amount, plugin) {
+    photoMB1 = $('#photo-stepper1').val() *.50;
+    moveBar();
+    totalmb1();
+});
+
+$('#video-stepper1').on('increase', function (e, amount, plugin) {
+    videoMb1 = $('#video-stepper1').val() * 25;
+    moveBar();
+    totalmb1();
+})
+
+$('#video-stepper1').on('decrease', function (e, amount, plugin) {
+    videoMb1 = $('#video-stepper1').val() * 25;
+    moveBar();
+    totalmb1();
+});
+
+$('#internet-stepper1').on('increase', function (e, amount, plugin) {
+    internetMb1 = $('#internet-stepper1').val() * .5;
+    moveBar();
+    totalmb1();
+})
+
+$('#internet-stepper1').on('decrease', function (e, amount, plugin) {
+    internetMb1 = $('#internet-stepper1').val() * .5;
+    moveBar();
+    totalmb1();
+});
 
   $('#song-slide1').slider({
     slide:function(event,ui){
@@ -258,21 +410,7 @@ function sliders(){
     }
   });
 
-  $('#video-slide1').slider({
-    slide:function(event,ui){
-      videoMb1 = ui.value *25;
-      moveBar();
-      totalmb1();
-    }
-  });
-
-  $('#internet-slide1').slider({
-    slide:function(event,ui){
-      internetMb1 = ui.value * .5;
-      moveBar();
-      totalmb1();
-    }
-  });
+ 
  
   $('#aplication-slide1').slider({
     slide:function(event,ui){
@@ -300,13 +438,42 @@ function sliders(){
   });
 
   /*************Usuario2*****************************/
-  $('#photo-slide2').slider({
-    slide:function(event,ui){
-      photoMB2 = ui.value * .50 ;
-      moveBar();
-      totalmb2();
-    }
-    });
+ 
+  $('#photo-stepper2').on('increase', function (e, amount, plugin) {
+    photoMB2 = $('#photo-stepper2').val() *.50;
+    moveBar();
+    totalmb2();
+})
+
+$('#photo-stepper2').on('decrease', function (e, amount, plugin) {
+    photoMB2 = $('#photo-stepper2').val() *.50;
+    moveBar();
+    totalmb2();
+});
+
+$('#video-stepper2').on('increase', function (e, amount, plugin) {
+    videoMb2 = $('#video-stepper2').val() * 25;
+    moveBar();
+    totalmb2();
+})
+
+$('#video-stepper2').on('decrease', function (e, amount, plugin) {
+    videoMb2 = $('#video-stepper2').val() * 25;
+    moveBar();
+    totalmb2();
+});
+
+$('#internet-stepper2').on('increase', function (e, amount, plugin) {
+    internetMb2 = $('#internet-stepper2').val() * .5;
+    moveBar();
+    totalmb2();
+})
+
+$('#internet-stepper2').on('decrease', function (e, amount, plugin) {
+    internetMb2 = $('#internet-stepper2').val() * .5;
+    moveBar();
+    totalmb2();
+});
 
   $('#song-slide2').slider({
     slide:function(event,ui){
@@ -317,21 +484,7 @@ function sliders(){
     }
   });
 
-  $('#video-slide2').slider({
-    slide:function(event,ui){
-      videoMb2 = ui.value *25;
-      moveBar();
-      totalmb2();
-    }
-  });
 
-  $('#internet-slide2').slider({
-    slide:function(event,ui){
-      internetMb2 = ui.value * .5;
-      moveBar();
-      totalmb2();
-    }
-  });
  
   $('#aplication-slide2').slider({
     slide:function(event,ui){
@@ -359,13 +512,41 @@ function sliders(){
   });
 
   /*************Usuario3*****************************/
-  $('#photo-slide3').slider({
-    slide:function(event,ui){
-      photoMB3 = ui.value * .50 ;
-      moveBar();
-      totalmb3();
-    }
-    });
+   $('#photo-stepper3').on('increase', function (e, amount, plugin) {
+    photoMB3 = $('#photo-stepper3').val() *.50;
+    moveBar();
+    totalmb3();
+})
+
+$('#photo-stepper3').on('decrease', function (e, amount, plugin) {
+    photoMB3 = $('#photo-stepper3').val() *.50;
+    moveBar();
+    totalmb3();
+});
+
+$('#video-stepper3').on('increase', function (e, amount, plugin) {
+    videoMb3 = $('#video-stepper3').val() * 25;
+    moveBar();
+    totalmb3();
+})
+
+$('#video-stepper3').on('decrease', function (e, amount, plugin) {
+    videoMb3 = $('#video-stepper3').val() * 25;
+    moveBar();
+    totalmb3();
+});
+
+$('#internet-stepper3').on('increase', function (e, amount, plugin) {
+    internetMb3 = $('#internet-stepper3').val() * .5;
+    moveBar();
+    totalmb3();
+})
+
+$('#internet-stepper3').on('decrease', function (e, amount, plugin) {
+    internetMb3 = $('#internet-stepper3').val() * .5;
+    moveBar();
+    totalmb3();
+});
 
   $('#song-slide3').slider({
     slide:function(event,ui){
@@ -375,21 +556,7 @@ function sliders(){
     }
   });
 
-  $('#video-slide3').slider({
-    slide:function(event,ui){
-      videoMb3 = ui.value *25;
-      moveBar();
-      totalmb3();
-    }
-  });
 
-  $('#internet-slide3').slider({
-    slide:function(event,ui){
-      internetMb3 = ui.value * .5;
-      moveBar();
-      totalmb3();
-    }
-  });
  
   $('#aplication-slide3').slider({
     slide:function(event,ui){
@@ -418,13 +585,41 @@ function sliders(){
 
 
   /*************Usuario4*****************************/
-  $('#photo-slide4').slider({
-    slide:function(event,ui){
-      photoMB4 = ui.value * .50 ;
-      moveBar();
-      totalmb4();
-    }
-    });
+   $('#photo-stepper4').on('increase', function (e, amount, plugin) {
+    photoMB4 = $('#photo-stepper4').val() *.50;
+    moveBar();
+    totalmb4();
+})
+
+$('#photo-stepper4').on('decrease', function (e, amount, plugin) {
+    photoMB4 = $('#photo-stepper4').val() *.50;
+    moveBar();
+    totalmb4();
+});
+
+$('#video-stepper4').on('increase', function (e, amount, plugin) {
+    videoMb4 = $('#video-stepper4').val() * 25;
+    moveBar();
+    totalmb4();
+})
+
+$('#video-stepper4').on('decrease', function (e, amount, plugin) {
+    videoMb4 = $('#video-stepper4').val() * 25;
+    moveBar();
+    totalmb4();
+});
+
+$('#internet-stepper4').on('increase', function (e, amount, plugin) {
+    internetMb4 = $('#internet-stepper4').val() * .5;
+    moveBar();
+    totalmb4();
+})
+
+$('#internet-stepper4').on('decrease', function (e, amount, plugin) {
+    internetMb4 = $('#internet-stepper4').val() * .5;
+    moveBar();
+    totalmb4();
+});
 
   $('#song-slide4').slider({
     slide:function(event,ui){
@@ -434,21 +629,6 @@ function sliders(){
     }
   });
 
-  $('#video-slide4').slider({
-    slide:function(event,ui){
-      videoMb4 = ui.value *25;
-      moveBar();
-      totalmb4();
-    }
-  });
-
-  $('#internet-slide4').slider({
-    slide:function(event,ui){
-      internetMb4 = ui.value * .5;
-      moveBar();
-      totalmb4();
-    }
-  });
  
   $('#aplication-slide4').slider({
     slide:function(event,ui){
@@ -477,33 +657,47 @@ function sliders(){
 
 
   /*************Usuario5*****************************/
-  $('#photo-slide5').slider({
-    slide:function(event,ui){
-      photoMB5 = ui.value * .50 ;
-      moveBar();
-      totalmb5();
-    }
-    });
+
+   $('#photo-stepper5').on('increase', function (e, amount, plugin) {
+    photoMB5 = $('#photo-stepper5').val() *.50;
+    moveBar();
+    totalmb5();
+})
+
+$('#photo-stepper5').on('decrease', function (e, amount, plugin) {
+    photoMB5 = $('#photo-stepper5').val() *.50;
+    moveBar();
+    totalmb5();
+});
+
+$('#video-stepper5').on('increase', function (e, amount, plugin) {
+    videoMb5 = $('#video-stepper5').val() * 25;
+    moveBar();
+    totalmb5();
+})
+
+$('#video-stepper5').on('decrease', function (e, amount, plugin) {
+    videoMb5 = $('#video-stepper5').val() * 25;
+    moveBar();
+    totalmb5();
+});
+
+$('#internet-stepper5').on('increase', function (e, amount, plugin) {
+    internetMb5 = $('#internet-stepper5').val() * .5;
+    moveBar();
+    totalmb5();
+})
+
+$('#internet-stepper5').on('decrease', function (e, amount, plugin) {
+    internetMb5 = $('#internet-stepper5').val() * .5;
+    moveBar();
+    totalmb5();
+});
+ 
 
   $('#song-slide5').slider({
     slide:function(event,ui){
       songMB5 = ui.value * 7;
-      moveBar();
-      totalmb5();
-    }
-  });
-
-  $('#video-slide5').slider({
-    slide:function(event,ui){
-      videoMb5 = ui.value *25;
-      moveBar();
-      totalmb5();
-    }
-  });
-
-  $('#internet-slide5').slider({
-    slide:function(event,ui){
-      internetMb5 = ui.value * .5;
       moveBar();
       totalmb5();
     }
@@ -569,9 +763,15 @@ function mostrar(){
   
 
    function crear(){
+   $( "#xDiaFoto1" ).prop( "checked", true );
+   $( "#xDiaFoto2" ).prop( "checked", true );
+   $( "#xDiaFoto3" ).prop( "checked", true );
+   $( "#xDiaFoto4" ).prop( "checked", true );
+   $( "#xDiaFoto5" ).prop( "checked", true );
+
     if(usuarioLength<6){
   $('#photo-steppe'+usuarioLength).inputStepper({
-    initialValue:2 ,
+    initialValue:0 ,
     min: 0,
     max: Infinity,
     selectorInput: 'input',
@@ -592,25 +792,29 @@ function mostrar(){
     }
   });
 
-  $('#video-slide'+usuarioLength).slider({
+   $('#video-steppe'+usuarioLength).inputStepper({
+    initialValue:0 ,
     min: 0,
-    max: 10,
-    value:0,
-    range:"min",
-    slide:function(event,ui){
-     
-    }
-  });
+    max: Infinity,
+    selectorInput: 'input',
+    selectorButtonIncrease: '[data-input-stepper-increase]',
+    selectorButtonDecrease: '[data-input-stepper-decrease]',
+    dataAttributeIncrease: 'input-stepper-increase',
+    dataAttributeDecrease: 'input-stepper-decrease',
+    classNameDisabled: 'is-disabled'
+});
 
-  $('#internet-slide'+usuarioLength).slider({
+    $('#internet-steppe'+usuarioLength).inputStepper({
+    initialValue:0 ,
     min: 0,
-    max: 10,
-    value:0,
-    range:"min",
-    slide:function(event,ui){
-      
-    }
-  });
+    max: Infinity,
+    selectorInput: 'input',
+    selectorButtonIncrease: '[data-input-stepper-increase]',
+    selectorButtonDecrease: '[data-input-stepper-decrease]',
+    dataAttributeIncrease: 'input-stepper-increase',
+    dataAttributeDecrease: 'input-stepper-decrease',
+    classNameDisabled: 'is-disabled'
+});
  
   $('#aplication-slide'+usuarioLength).slider({
     min: 0,
@@ -624,7 +828,7 @@ function mostrar(){
 
   $('#email-slide'+usuarioLength).slider({
     min: 0,
-    max: 10,
+    max: 1000,
     value:0,
     range:"min",
     slide:function(event,ui){
